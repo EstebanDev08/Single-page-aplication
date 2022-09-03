cb3559970aa7d2d856b22a72bb4392102c95cab0
elmejormaxsimus320@gmail.com <estebansolarte86@gmail.com>
2022-09-03 15:28:54 -0500

    primer commit

---
+++ src/pages/Character.js
@@ -0,0 +1,24 @@
+const Character = () => {
+    const view = /*html*/ `
+
+<div class="Characters-innner">
+    <article class="Characters-card">
+        <img src="image" alt="name">
+        <h2>name</h2>
+    </article>
+    <article class="Characters-card">
+        <h3>episodes:</h3>
+        <h3>status:</h3>
+        <h3>Species:</h3>
+        <h3>Gender:</h3>
+        <h3>Last location:</h3>
+    </article>
+</div>
+
+`;
+    return view
+
+};
+
+
+export default Character
---
+++ src/tamplate/Header.js
@@ -0,0 +1,27 @@
+const Header = () => {
+    const view = `
+    
+    <div class="Header-main"> 
+        <div class="Header-logo">
+            <h1>
+                <a href="/">
+                    pickle rick
+                </a>
+            </h1>
+        </div>
+        <div class="Header/nav">
+
+            <a href="#/about/">
+                about
+            </a>
+
+        </div>
+    </div>
+    
+    `;
+    return view
+
+};
+
+
+export default Header
